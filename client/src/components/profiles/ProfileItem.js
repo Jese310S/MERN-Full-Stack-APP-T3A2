ae20ecb0dd64d6f2ffadb419976c65e4a4bafa36
Jese310S <jess.sole710@gmail.com>
2021-01-16 12:10:15 +1100

    create post actions and types

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const ProfileItem = ({
-    profile: {
-      user: { _id, name, avatar },
-      status,
-      company,
-      location,
-      skills
-    }
+  profile: {
+    user: { _id, firstname, avatar },
+    status,
+    company,
+    location,
+    skills
+  }
   }) => {
       return  <div className='profile bg-light'>
       <img src={avatar} alt='' className='round-img' />
+
       <div>
-        <h2>{name}</h2>
+        <h2>{firstname}</h2>
         <p>
           {status} {company && <span> at {company}</span>}
         </p>
